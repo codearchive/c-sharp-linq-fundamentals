9988f4767d28d68c750cba01ddeaea609e3f1a29
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-12 16:53:28 +0300

    Completed unit 4.3 - Creating an Operator with Yield Return

--- module_04/module_04/Movie.cs
+++ module_04/module_04/Movie.cs
@@ -1,9 +1,22 @@
-﻿namespace module_04
+﻿using System;
+
+namespace module_04
 {
     public class Movie
     {
         public string Title { get; set; }
         public float Rating { get; set; }
-        public int Year { get; set; }
+
+        private int _year;
+
+        public int Year
+        {
+            get
+            {
+                Console.WriteLine($"Returning {_year} for {Title}");
+                return _year;
+            }
+            set => _year = value;
+        }
     }
 }
--- module_04/module_04/Program.cs
+++ module_04/module_04/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Linq;
+using System.Linq;
+
 
 namespace module_04
 {
@@ -8,20 +9,20 @@ namespace module_04
     {
         public static void Main(string[] args)
         {
-            var movies = new List<Movie>()
+            var movies = new List<Movie>
             {
-                new Movie { Title = "The Dark Knight", Rating=8.9f, Year = 2008 },
-                new Movie { Title = "Star Wars V", Rating = 8.7f, Year = 1980 },
-                new Movie { Title = "Casablanca", Rating=8.5f, Year = 1942 },
+                new Movie { Title = "The Dark Knight",   Rating = 8.9f, Year = 2008 },
+                new Movie { Title = "Star Wars V",       Rating = 8.7f, Year = 1980 },
+                new Movie { Title = "Casablanca",        Rating = 8.5f, Year = 1942 },
                 new Movie { Title = "The King's Speech", Rating = 8.0f, Year = 2010 }
             };
 
             var query = movies.Filter(m => m.Year > 2000);
 
-            foreach (var movie in query)
-            {
-                Console.WriteLine(movie.Title);
-            }
+            //foreach (var movie in query)
+            //{
+            //    Console.WriteLine(movie.Title);
+            //}
         }   
     }
 }
