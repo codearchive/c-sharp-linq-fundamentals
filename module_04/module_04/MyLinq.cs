30a24f0b4e708d0527f707b889c4ee4a60b0adde
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-14 18:35:26 +0300

    Completed unit 4.9 - Querying Infinity

--- module_04/module_04/MyLinq.cs
+++ module_04/module_04/MyLinq.cs
@@ -5,6 +5,14 @@ namespace module_04
 {
     public static class MyLinq
     {
+        public static IEnumerable<double> Random()
+        {
+            var random = new Random();
+            while (true)
+            {
+                yield return random.NextDouble();
+            }
+        }
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
             foreach (var item in source)
--- module_04/module_04/Program.cs
+++ module_04/module_04/Program.cs
@@ -9,6 +9,12 @@ namespace module_04
     {
         public static void Main(string[] args)
         {
+            var numbers = MyLinq.Random().Where(n => n > 0.5).Take(10);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
             var movies = new List<Movie>
             {
                 new Movie { Title = "The Dark Knight",   Rating = 8.9f, Year = 2008 },
