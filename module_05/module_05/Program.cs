3f907029b37d85ee94c45e1e6a8707edf5af3dc1
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-14 23:52:03 +0300

    Completed unit 5.4 - Implementing a File Processor

--- module_05/module_05/Program.cs
+++ module_05/module_05/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace module_05
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace module_05
 {
     class Program
     {
         static void Main()
         {
+            var cars = ProcessFile("fuel.csv");
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Name);
+            }
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            return File.ReadAllLines(path)
+                       .Skip(1)
+                       .Where(line => line.Length > 1)
+                       .Select(Car.ParseFromCsv)
+                       .ToList();
         }
     }
 }
